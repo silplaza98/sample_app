6c60b8ef8bfc45ba35fb2c2f587511a8feb5035a
Ubuntu <ubuntu@ip-172-31-87-175.ec2.internal>
2019-05-23 12:51:55 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,43 +1,16 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
-    validates:name,presence:true
-    validates :email, presence: true
-    validates:name,presence:true,length:{maximum:50}
-    validates:email,presence:true,length:{maximum:255}
-    validates:name,presence:true,length:{maximum:50}
-    VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates:email,presence:true,length:{maximum:255},format:{with:VALID_EMAIL_REGEX}
-    validates:email,presence:true,length:{maximum:255},format:{with:VALID_EMAIL_REGEX},uniqueness:{case_sensitive:false}
-    before_save { self.email = self.email.downcase }
-    has_secure_password
-     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    
-    def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-    end
-    
-    def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-    end
-    
-    def User.new_token
-    SecureRandom.urlsafe_base64
-    end
-    
-    def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-    end
-    
-    def self.new_token
-    SecureRandom.urlsafe_base64
-    end
-    
-    class << self
+   attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+   class << self
     # Returns the hash digest of the given string.
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -49,15 +22,49 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
-    end
-    
-    def authenticated?(remember_token)
+  end
+  
+   def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-    
-    def forget
-    update_attribute(:remember_digest, nil)
-    end
+  end
   
+   def forget
+     update_attribute(:remember_digest, nil)
+   end
+   
+   # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+   
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
